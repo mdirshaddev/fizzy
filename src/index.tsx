d12ede7dd39a7085ff8998063e8ebdbfa7c9d0f8
Md Irshad <mdirshaddev@gmail.com>
2022-06-07 05:44:20 +0530

    chore: folder structure and and files update
    * deleted boilerplate code
    * moving of files to different folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
+import 'styles/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from 'serviceWorkerRegistration';
+import reportWebVitals from 'reportWebVitals';
+import App from 'App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
